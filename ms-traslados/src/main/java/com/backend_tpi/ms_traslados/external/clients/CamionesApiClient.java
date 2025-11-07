af8b4915164478b2f76a168966a04d9e1ee25b82
Matías Aarón Paná <matiasaaron.pana@cba.gov.ar>
2025-11-07 17:00:13 -0300

    feat: external errors handlers

--- ms-traslados/src/main/java/com/backend_tpi/ms_traslados/external/clients/CamionesApiClient.java
+++ ms-traslados/src/main/java/com/backend_tpi/ms_traslados/external/clients/CamionesApiClient.java
@@ -1,7 +1,9 @@
 package com.backend_tpi.ms_traslados.external.clients;
 
+import com.backend_tpi.ms_traslados.exceptions.BaseException;
 import com.backend_tpi.ms_traslados.external.dtos.responses.CiudadProvinciaDtoRes;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -22,7 +24,19 @@ public class CamionesApiClient {
   public CiudadProvinciaDtoRes getCiudadProvincia(Long idCiudad) {
     return camionesRestClient.get()
         .uri("/api/ciudades/{idCiudad}", idCiudad)
-        .retrieve() // Acá es donde se ejecuta la petición
+        .retrieve() // Acá es donde realmente se ejecuta la petición (IMPORTANTE)
+        .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+          int status = response.getStatusCode().value();
+
+          if (status == 404) {
+            throw BaseException.notFoundById("Ciudad", idCiudad);
+          }
+          throw BaseException.badRequest("Error al obtener la ciudad");
+
+        })
+        .onStatus(HttpStatusCode::is5xxServerError, (request, response) -> {
+          throw BaseException.internalError("Error al obtener la ciudad");
+        })
         .body(CiudadProvinciaDtoRes.class);
   }
 }
