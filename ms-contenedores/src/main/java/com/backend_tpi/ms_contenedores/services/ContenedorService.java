c15509a9ffc0a58394252305d83e6435c1e0ba6d
fabrizio <96095@sistemas.frc.utn.edu.ar>
2025-11-16 11:14:33 -0300

    feat: creacion endpoint PUT para actualizar coordenadas de un contenedor

--- ms-contenedores/src/main/java/com/backend_tpi/ms_contenedores/services/ContenedorService.java
+++ ms-contenedores/src/main/java/com/backend_tpi/ms_contenedores/services/ContenedorService.java
@@ -8,10 +8,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.backend_tpi.ms_contenedores.constants.EstadoContenedor;
+import com.backend_tpi.ms_contenedores.dtos.CoordenadasDTO;
 import com.backend_tpi.ms_contenedores.dtos.EstadoDTO;
 import com.backend_tpi.ms_contenedores.dtos.PesoVolumenDTO;
 import com.backend_tpi.ms_contenedores.models.Contenedores;
 import com.backend_tpi.ms_contenedores.repositories.ContenedorRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ContenedorService {
@@ -76,5 +79,20 @@ public class ContenedorService {
                     HttpStatus.NOT_FOUND, "Contenedor no encontrado"));
 
     return new EstadoDTO(contenedor.getEstadoContenedores().name());
+    }
+
+    @Transactional
+    public void actualizarCoordenadas(Long id, CoordenadasDTO coordenadas) {
+        if (coordenadas == null || coordenadas.getLatitud() == null || coordenadas.getLongitud() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Coordenadas inválidas");
+        }
+        Contenedores contenedor = contenedorRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Contenedor no encontrado"));
+        
+        contenedor.setLatitud(coordenadas.getLatitud());
+        contenedor.setLongitud(coordenadas.getLongitud());
+
+        contenedorRepository.save(contenedor);
     }
 }
